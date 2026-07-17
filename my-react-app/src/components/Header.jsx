6ee3950b8a9a15b524c85f2cd6fbba42b666747e
Amal krishna <uvamal11@gmail.com>
2026-07-17 22:38:15 +0530

    17-6-2026

---
+++ my-react-app/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return (
+        <header style={{padding:"10px",background:"#f4f4f4",marginBottom:"20px"}}>
+            <h1>Class room dashboard</h1>
+        </header>
+    )
+}
