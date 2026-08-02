5542d6eafeba43e0dfe81619dccb809b10d241ac
Amal krishna <uvamal11@gmail.com>
2026-08-02 21:23:59 +0530

    practicals

--- react-app/react-app/src/App.jsx
+++ react-app/react-app/src/App.jsx
@@ -31,24 +31,109 @@
 //================
 
 
-import {useState}  from "react";
+// import {useState}  from "react";
 
-function Counter(){
-
-
-  const [state,setCount]=useState(0);
-
-  return (
+// function Counter(){
 
 
-    <>
-    <h1>Count:{state}</h1>
+//   const [state,setCount]=useState(0);
 
-    <button onClick={()=>setCount(state+1)}>Increase</button>
+//   return (
+
+
+//     <>
+//     <h1>Count:{state}</h1>
+
+//     <button onClick={()=>setCount(state+1)}>Increase</button>
     
-    </>
+//     </>
 
-  )
+//   )
+// }
+
+// export default Counter;
+
+//====================
+
+
+
+
+// function Student(){
+
+// const students = ["Amal", "Rahul", "Anu"];
+
+// return (
+
+//   <ul>
+
+//     {students.map((stu,index)=>{
+//       return <li key={index}>{stu}</li>
+      
+//     })}
+    
+//   </ul>
+
+// )
+
+// }
+
+
+// export default Student;
+
+
+//=======================
+
+
+
+import {useEffect,useState}  from "react";
+
+
+
+function App(){
+
+
+  const [users,setdata]=useState([]);
+
+
+useEffect(()=>{
+
+
+const fetchdata=async()=>{
+
+
+try  {
+
+  const response=await fetch("https://jsonplaceholder.typicode.com/users");
+
+  const data=await response.json();
+
+  setdata(data)
+}catch(error){
+  console.error(error);
 }
 
-export default Counter;
+}
+
+
+fetchdata()
+
+
+},[]);
+
+return (
+
+  <div>
+
+{
+users.map((user)=> <p  key={user.id}>{user.name}</p>)
+
+}
+
+
+  </div>
+)
+
+}
+
+
+export default App;
