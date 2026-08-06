f8aaf39e2e48eaae1b3e17b01fe41b7f930eff62
Amal krishna <uvamal11@gmail.com>
2026-08-06 23:20:59 +0530

    practise

--- react-app/react-app/src/App.jsx
+++ react-app/react-app/src/App.jsx
@@ -1,343 +1,42 @@
-// function Student(){
-//   return (
-//     <>
-//     <h1>Name:Amal</h1>
-//     <h2>Age:29</h2>
-//     </>
-//   )
-// }
-
-// export default Student;
-
-//======================================
 
 
-// function Student(props){
-//   return <>
-//   <h1>Name:{props.name}</h1>
-//   <h2>age:{props.age}</h2>
-//   </>
-// }
+function child(){
 
 
-// function App(){
-//   return (
-//     <Student name="amal" age={29}/>
-//   )
-// }
+  const user={
 
-// export default App;
-
-//================
-
-
-// import {useState}  from "react";
-
-// function Counter(){
-
-
-//   const [state,setCount]=useState(0);
-
-//   return (
-
-
-//     <>
-//     <h1>Count:{state}</h1>
-
-//     <button onClick={()=>setCount(state+1)}>Increase</button>
+    name :"Amal"
     
-//     </>
+  }
 
-//   )
-// }
 
-// export default Counter;
+  return (
 
-//====================
+    <parent user={user}/>
 
+  )
 
 
-
-// function Student(){
-
-// const students = ["Amal", "Rahul", "Anu"];
-
-// return (
-
-//   <ul>
-
-//     {students.map((stu,index)=>{
-//       return <li key={index}>{stu}</li>
-      
-//     })}
-    
-//   </ul>
-
-// )
-
-// }
-
-
-// export default Student;
-
-
-//======================================================
-
-
-
-// import {useEffect,useState}  from "react";
-
-
-
-// function App(){
-
-
-//   const [users,setdata]=useState([]);
-
-
-// useEffect(()=>{
-
-
-// const fetchdata=async()=>{
-
-
-// try  {
-
-//   const response=await fetch("https://jsonplaceholder.typicode.com/users");
-
-//   const data=await response.json();
-
-//   setdata(data)
-// }catch(error){
-//   console.error(error);
-// }
-
-// }
-
-
-// fetchdata()
-
-
-// },[]);
-
-// return (
-
-//   <div>
-
-// {
-// users.map((user)=> <p  key={user.id}>{user.name}</p>)
-
-// }
-
-
-//   </div>
-// )
-
-// }
-
-
-// export default App;
-
-//========================================================useref
-
-
-
-
-// import {useRef}  from "react";
-
-// function InputFocus(){
-
-// let input=useRef(null);
-
-// let handlefoc=()=>{
-// input.current.focus();
-
-// console.log(input.current.value)
-// }
-
-
-// return (
-
-//   <>
-  
-//   <input type="text"  ref={input} />
-
-
-//       <br />
-//       <br />
-
-//   <button onClick={handlefoc}> focus on input</button>
-  
-//   </>
-
-// )
-
-// }
-
-// export default InputFocus;
-
-//======================================
-
-// import {useRef}  from "react";
-
-
-// function InputValue(){
-
-//   let inputref=useRef(null);
-
-//   let showfun=()=>{
-//     console.log(inputref.current.value)
-//   }
-
-
-//   return (
-
-//     <>
-    
-//     <input type="text"  ref={inputref} />
-
-//     <br/>
-//     <br/>
-
-//     <button  onClick={showfun}> click here to show</button>
-    
-//     </>
-
-//   )
-// }
-
-// export default InputValue;
-
-//==================
-
-
-// import {useState}  from "react";
-
-
-// function InputExample(){
-
-//   const [state,setState]=useState(" ");
-
-//   return (
-
-// <>
-
-// <input type="text"  onChange={(event)=>setState(event.target.value)}/>
-
-// <p>{state}</p>
-
-// </>
-
-//   )
-// }
-
-
-
-// export default InputExample;
-
-//=======================
-
-
-// import {BrowserRouter,Routes,Route,Link} from "react-router-dom";
-
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-
-// function App(){
-//   return (
-
-//     <BrowserRouter>
-    
-//     <nav>
-//       <Link to="/">Home</Link>
-
-
-//       {"|"}
-//       <Link to="/about">About</Link>
-//     </nav>
-
-
-// <Routes>
-// <Route path="/" element={<Home/>}></Route>
-// <Route path="/about" element={<About/>}></Route>
-
-
-// </Routes>
-
-
-
-
-//     </BrowserRouter>
-//   )
-// }
-
-// export default App;
-
-//=================
-
-// import {BrowserRouter,Routes,Route}  from "react"
-
-// import Login from "./pages/Login";
-// import Home from "./pages/Home";
-
-// function App(){
-
-// return (
-
-//   <>
-//   <BrowserRouter>
-//   <Routes>
-
-
-// <Route path="/" element={<Home/>}></Route>
-// <Route  path="/Login" element={<Login/>}></Route>
-
-//   </Routes>
-  
-  
-//   </BrowserRouter>
-  
-  
-//   </>
-// ) 
-
-// }
-
-// export default App;
-
-//===============================
-
-
-// import { useState } from "react";
-
-// function App() {
-
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-//   return (
-//     <>
-//       <p>
-//         {isLoggedIn ? "Welcome, User!" : "Please Login"}
-//       </p>
-
-//       <button
-//         onClick={() => setIsLoggedIn(!isLoggedIn)}
-//       >
-//         {isLoggedIn ? "Logout" : "Login"}
-//       </button>
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-//==================
-
-
-import {useState}  from "react";
-
-
-function App(){
-  const [Email]
 }
+
+
+
+
+
+
+
+
+function parent({user}){
+
+const {name} =user;
+
+return (
+
+  <>
+  <h1>{name}</h1>
+  </>
+)
+
+}
+
+export default parent;
